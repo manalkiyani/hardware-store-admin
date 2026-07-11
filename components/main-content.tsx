459a8db7d87450f053aacd237a4c795d8dec168a
manalkiyani <manalkiyani687@gmail.com>
2026-07-12 00:21:18 +0500

    Double bottom padding to pb-32

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -10,7 +10,7 @@ export default function MainContent({ children }: { children: React.ReactNode })
       className="min-h-screen transition-all duration-200"
       style={{ marginLeft: collapsed ? "4rem" : "14rem" }}
     >
-      <div className="max-w-screen-2xl mx-auto px-12 pt-6 pb-16">{children}</div>
+      <div className="max-w-screen-2xl mx-auto px-12 pt-6 pb-32">{children}</div>
     </main>
   );
 }
